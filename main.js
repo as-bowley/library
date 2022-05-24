3d961ffb36a046eb11d43a0067a38e99c5a2a48d
as-bowley <as.bowley@gmail.com>
2022-05-24 22:49:53 +0200

    add form and code to receive form data in to array

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const openFormButton = document.querySelector(".add-book-button").addEventListen
 const closeFormButton = document.querySelector(".close-button").addEventListener('click', closeForm);
 const formOverlay = document.getElementById("overlay");
 const form = document.querySelector(".pop-up-form")
+const formSubmit = document.querySelector(".submit").addEventListener('click', submitForm);
 
 const myLibrary = [];
 
@@ -24,13 +25,14 @@ function addBook(obj) {
 function displayBook() {
     for (let i = 0; i < myLibrary.length; i++) {
         const div = document.createElement('div');
+        div.classList.add('book-entry-div')
         div.innerHTML = 
         '<p>' + 'Title: ' + myLibrary[i].title + '<br>' +
         '<p>' + 'Author: ' + myLibrary[i].author + '<br>' +
         '<p>' + 'Length: ' + myLibrary[i].length + '<br>' +
         '<p>' + 'Have read: ' + myLibrary[i].read;
         bookDisplayContainer.appendChild(div);
-    }                      
+    } 
 }
 
 function openForm() {
@@ -41,6 +43,33 @@ function openForm() {
 function closeForm() {
     form.classList.remove('active');
     formOverlay.classList.remove('active');
+}
+
+function resetDisplay() {
+    const currentDivs = document.getElementById("book-container");
+    currentDivs.innerHTML = "";
+}
+
+function resetForm() {
+    document.getElementById('title').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('length').value = "";
+    document.getElementById('read-status').value = "";
+}
+
+function submitForm() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const length = document.getElementById('length').value;
+    const readStatus = document.getElementById('read-status').value;
+    
+    newBook = new Book(title, author, length, readStatus);
+
+    myLibrary.push(newBook);
+    resetDisplay()
+    displayBook();
+    resetForm();
+    closeForm();
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '500 Pages', false);
